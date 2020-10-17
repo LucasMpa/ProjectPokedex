69eb681922ea0cf3f189e45262578d3360ceb423
Lucas Matheus <lucasmpa16@gmail.com>
2020-10-17 02:56:01 -0300

    Updates

---
+++ precache-manifest.99e9735aa97e2baa5c50baabd2bb8d72.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0c21fa5fbb9779202a15a28c0adb627",
+    "url": "/index.html"
+  },
+  {
+    "revision": "800c50337471f0748292",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f76010708ed18809e1d4",
+    "url": "/static/js/2.cc040993.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.cc040993.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "800c50337471f0748292",
+    "url": "/static/js/main.254858a1.chunk.js"
+  },
+  {
+    "revision": "250103716dfc89e5abb8",
+    "url": "/static/js/runtime-main.9ff9079d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
